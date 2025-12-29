64a1c9806135bd360e167681ba6e46a96b7d756b
HiddenCirno <2301697863@qq.com>
2025-12-30 00:22:46 +0800

    添加了掉宝狗牌样式, 添加了PMC狗牌相关参数

--- obj/Debug/火神重工-无限.AssemblyInfo.cs
+++ obj/Debug/火神重工-无限.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+3b9d0d12462c11245529990c24f2152b472aa0e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+373c1d8cb346d5ef1da2207e6f6fd5e4bce16b3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyTitleAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
