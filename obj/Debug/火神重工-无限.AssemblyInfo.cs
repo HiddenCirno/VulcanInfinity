3bb1e24698e7845d455387473c1933933d9bfffd
HiddenCirno <2301697863@qq.com>
2025-12-31 20:29:18 +0800

    ash12限制解除, 新配方添加, 具体的懒得统计了

--- obj/Debug/火神重工-无限.AssemblyInfo.cs
+++ obj/Debug/火神重工-无限.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+64a1c9806135bd360e167681ba6e46a96b7d756b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+609648c6c5f36abb674a8df7ace23523d6748457")]
 [assembly: System.Reflection.AssemblyProductAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyTitleAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
