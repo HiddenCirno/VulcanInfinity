72d2ccf268fd3855574fe0aeba27c26d626ac9ff
HiddenCirno <2301697863@qq.com>
2026-01-14 01:30:23 +0800

    忘了改FixId了....

--- obj/Debug/火神重工-无限.AssemblyInfo.cs
+++ obj/Debug/火神重工-无限.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+436e73a3a90cff0fac286721c085935c2c3ee209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+dbd472a5408f937bea5eedf53337461122516614")]
 [assembly: System.Reflection.AssemblyProductAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyTitleAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
