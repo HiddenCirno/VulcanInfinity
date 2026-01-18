74ed934e75991f301d9ddc1faa920096c5f112f6
HiddenCirno <2301697863@qq.com>
2026-01-18 22:02:50 +0800

    修个子弹兼容

--- obj/Debug/火神重工-无限.AssemblyInfo.cs
+++ obj/Debug/火神重工-无限.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+35e43e5b8dd6c201c91d99a76626a2813c0841ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1+1a7f53f3f62e08cbdb0d4cecb701edd790c00d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyTitleAttribute("火神重工-无限")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
